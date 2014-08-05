b7885e1bb00c60c06e527dda190bd0c8cc48d8a2
Albeoris <albeoris@gmail.com>
2014-08-05 22:04:53 +0400

    Перенос в\из каталога CVS

--- Esthar/MainWindow/Locations/LocationsListTools.xaml.cs
+++ Esthar/MainWindow/Locations/LocationsListTools.xaml.cs
@@ -1,6 +1,9 @@
 using System;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Windows;
+using Esthar.Core;
 using Esthar.Data.Transform;
 using Esthar.UI;
 
@@ -32,7 +35,7 @@ namespace Esthar
             }
         }
 
-        private void OnUploadButtonClick(object sender, RoutedEventArgs e)
+        private void OnExportButtonClick(object sender, RoutedEventArgs e)
         {
             try
             {
@@ -42,6 +45,82 @@ namespace Esthar
             {
                 UIHelper.ShowError(ex);
             }
+        }
+
+        private void OnInboxButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int filesCopied = 0;
+                DirectoryInfo source = new DirectoryInfo(Options.WorkingDirectory);
+                DirectoryInfo target = new DirectoryInfo(Options.CVSDirectory);
+                SyncFolder(source, target, ref filesCopied);
+                AfterSyncFolder(source, target, filesCopied);
+            }
+            catch (Exception ex)
+            {
+                UIHelper.ShowError(ex);
+            }
+        }
+
+        private void OnOutboxButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int filesCopied = 0;
+                DirectoryInfo source = new DirectoryInfo(Options.CVSDirectory);
+                DirectoryInfo target = new DirectoryInfo(Options.WorkingDirectory);
+                SyncFolder(source, target, ref filesCopied);
+                AfterSyncFolder(source,target,filesCopied);
+            }
+            catch (Exception ex)
+            {
+                UIHelper.ShowError(ex);
+            }
+        }
+
+        private void SyncFolder(DirectoryInfo source, DirectoryInfo target, ref int filesCount)
+        {
+            FileInfo[] sourceFiles = source.GetFiles();
+            FileInfo[] targetFiles = target.GetFiles();
+
+            foreach (FileInfo sourceFile in sourceFiles)
+            {
+                if (sourceFile.Name[0] == '.')
+                    continue;
+
+                FileInfo targetFile = targetFiles.FirstOrDefault(tf => tf.Name.Equals(sourceFile.Name, StringComparison.OrdinalIgnoreCase));
+                if (targetFile == null || targetFile.LastWriteTimeUtc != sourceFile.LastWriteTimeUtc || sourceFile.Length != targetFile.Length)
+                {
+                    sourceFile.CopyTo(Path.Combine(target.FullName, sourceFile.Name));
+                    filesCount++;
+                }
+            }
+
+            DirectoryInfo[] sourceDirectories = source.GetDirectories();
+            foreach (DirectoryInfo sourceDirectory in sourceDirectories)
+            {
+                if(sourceDirectory.Name[0] == '.')
+                    continue;
+
+                SyncFolder(sourceDirectory, target.CreateSubdirectory(sourceDirectory.Name), ref filesCount);
+            }
+        }
+
+        private void AfterSyncFolder(DirectoryInfo source, DirectoryInfo target, int filesCopied)
+        {
+            StringBuilder sb = new StringBuilder(1024);
+            
+            sb.AppendLine("Изменения из каталога:");
+            sb.AppendLine(source.FullName);
+            sb.AppendLine();
+            
+            sb.AppendLine("Успешно перенесены в каталог:");
+            sb.AppendLine(target.FullName);
+            sb.AppendLine();
+
+            sb.AppendFormat("Количество измененных файлов: {0}", filesCopied);
+            MessageBox.Show(sb.ToString(), "Синхронизация", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
